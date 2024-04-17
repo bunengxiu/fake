7f7436dd11f67f3ab395dee7140696b9e8f1ea66
bunengxiu <bunengxiu@163.com>
2024-04-17 14:09:20 +0800

    result

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ url_prefix = f'http://127.0.0.1:{port}/fake'
 
 @app.get("/")
 async def root():
-    return {"message": "fake", 'version': '0.0.2'}
+    return {"message": "fake", 'version': '0.0.3'}
 
 
 @app.get("/fake/category")
